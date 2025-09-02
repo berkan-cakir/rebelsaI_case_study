ea43aac492619fc46c6f0c8d91e09ec218ccf0e5
Berkan Cakir <b.cakir@proton.me>
2025-09-02 23:52:04 +0200

    Add routing for document_classication API

--- api/app/routes/__init__.py
+++ api/app/routes/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .folder_insights import router as folder_insights_router
+from .document_classification import router as document_classification_router
 
 router = APIRouter()
 router.include_router(folder_insights_router, prefix="/folder-insights", tags=["Folder Insights"])
+router.include_router(document_classification_router, prefix="/document-classification", tags=["Document Classification"])
