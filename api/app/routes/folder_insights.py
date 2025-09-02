a7e33735968224099b2082f07b9d00f460d7f9a4
Berkan Cakir <b.cakir@proton.me>
2025-09-02 14:38:29 +0200

    Remove debugging code

--- api/app/routes/folder_insights.py
+++ api/app/routes/folder_insights.py
@@ -8,15 +8,6 @@ BASE_PATH = os.path.join(os.path.dirname(__file__), "Client Data")
 
 @router.get("/{folder_path:path}/count_files")
 async def folder_count(folder_path: str):
-    # full_path = os.path.normpath(os.path.join(BASE_PATH, folder_path))
-    # if not full_path.startswith(BASE_PATH):
-    #     raise HTTPException(status_code=400, detail="Invalid folder path")
-
-    # li = os.listdir(full_path)  # Debug line to check if BASE_PATH is correct
-
-    # files = [f for f in os.listdir(full_path)]
-    # return {"files": files}
-
     try:
         full_path = os.path.normpath(os.path.join(BASE_PATH, folder_path))
         if not full_path.startswith(BASE_PATH):
@@ -26,8 +17,8 @@ async def folder_count(folder_path: str):
         if count is not None:
             return {"folder_path": folder_path, "document_count": count}
 
-        # if not os.path.exists(folder_path):
-        #     raise HTTPException(status_code=404, detail="Folder not found")
+        if not os.path.exists(full_path):
+            raise HTTPException(status_code=404, detail="Folder not found")
         
         job_id = create_job(status="PENDING")
         update_folder_count.delay(job_id, full_path)
